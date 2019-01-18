7180974929e885ea71ed7e553619779421a76283
Jinho Hong <tpdleps@gmail.com>
2019-01-18 16:46:12 +0900

    remote resolver

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import 'reflect-metadata';
-import { ApolloServer } from 'apollo-server-express';
-import Express from 'express';
-import { buildSchema, formatArgumentValidationError } from 'type-graphql';
-import dotenv from 'dotenv';
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import Express from "express";
+import { buildSchema, formatArgumentValidationError } from "type-graphql";
+import dotenv from "dotenv";
 
 // import serverless from "serverless-http";
 
-import { CreateUserResolver } from './modules/user/createUser';
+import { CreateUserResolver } from "./modules/user/createUser";
 
 const app = Express();
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: "../.env" });
 const main = async () => {
   const port = process.env.PORT || 4000;
   const schema = await buildSchema({
-    resolvers: [CreateUserResolver]
+    resolvers: [__dirname + "/modules/**/*.ts"]
   });
 
   const apolloServer = new ApolloServer({
